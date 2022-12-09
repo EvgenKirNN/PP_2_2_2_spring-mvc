7a50d7cb28f6b43704f54a2b49f80048df4fa0ed
EvgenKirNN <rf@nxt.ru>
2022-12-10 00:51:01 +0300

    added carService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,29 +1,23 @@
 package web.controller;
 
-import web.model.Car;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 public class CarController {
 
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap model) {
-
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("VAZ", 2101, "white"));
-        cars.add(new Car("GAZ", 3310, "brown"));
-        cars.add(new Car("UAZ", 3151, "gray"));
-        cars.add(new Car("VAZ", 2110, "black"));
-        cars.add(new Car("ZAZ", 1102, "yellow"));
-
-        model.addAttribute("cars", (count == null || count >= 5) ? cars : cars.stream().limit(count).toList());
-
+        model.addAttribute("cars", (count == null || count >= 5) ? carService.getCars() : carService.getCars().stream().limit(count).toList());
         return "cars";
     }
 }
---
+++ src/main/java/web/service/CarService.java
@@ -0,0 +1,26 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+
+    List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("VAZ", 2101, "white"));
+        cars.add(new Car("GAZ", 3310, "brown"));
+        cars.add(new Car("UAZ", 3151, "gray"));
+        cars.add(new Car("VAZ", 2110, "black"));
+        cars.add(new Car("ZAZ", 1102, "yellow"));
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
